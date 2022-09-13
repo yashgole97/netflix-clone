4b4eafe3cd001e3ba1d742c091c4f20a8aeda041
yashgole97 <yashgole97@gmail.com>
2022-09-14 02:51:00 +0530

    netflix clone

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import Row from  './Row';
+import Banner from './Banner';
+import Nav from './Nav';
+import './App.css';
+import requests from './requests';
+
+function App() {
+  return (
+    <div className="App">
+      <Nav />
+      <Banner/>
+      <Row 
+        title="NETFLIX ORIGINALS" 
+        fetchUrl={requests.fetchNetflixOriginals}
+        isLargeRow
+      />
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending}/>
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated}/>
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies}/>
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies}/>
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies}/>
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies}/>
+      <Row title="Documentries" fetchUrl={requests.fetchDocumentaries}/>
+    </div>  
+  
+  );
+}
+
+export default App;
